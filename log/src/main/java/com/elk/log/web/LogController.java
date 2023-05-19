71eae6ad2c9629192986d24d846b3bff166e1f68
Dasom Kim <ektha115@gmail.com>
2023-05-19 17:32:35 +0900

    cc

--- log/src/main/java/com/elk/log/web/LogController.java
+++ log/src/main/java/com/elk/log/web/LogController.java
@@ -1,8 +1,18 @@
 package com.elk.log.web;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elk.log.elastic.ElasticDto;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.ElasticsearchException;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 public class LogController {
 	
 	public final LogService logService;
+	public final ElasticsearchClient client;
 	
 	@GetMapping("/log")
 	public String createLog() {
@@ -25,6 +36,17 @@ public class LogController {
 		log.info("findAll");
 		logService.findAll();
 		return "success";
+	}
+	
+	@GetMapping("/log/test")
+	public String test() throws ElasticsearchException, IOException {
+		SearchRequest sr = SearchRequest.of(s -> s.index("test5"));
+		SearchResponse<ElasticDto> sre = client.search(sr, ElasticDto.class);
+		List<Hit<ElasticDto>> hits = sre.hits().hits();
+		for(Hit<ElasticDto> object: hits) {
+			System.out.println(object.source());
+		}
+		return "test";
 	}
 	
 }
