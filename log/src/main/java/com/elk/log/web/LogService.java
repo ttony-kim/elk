f6aa45ff090e670db6bccbd295b5007280b186d2
Dasom Kim <ektha115@gmail.com>
2023-05-19 17:53:10 +0900

    pageable

--- log/src/main/java/com/elk/log/web/LogService.java
+++ log/src/main/java/com/elk/log/web/LogService.java
@@ -1,5 +1,7 @@
 package com.elk.log.web;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.elk.log.elastic.ElasticDto2;
@@ -20,5 +22,9 @@ public class LogService {
 			System.out.println(dto.toString());
 		}
 		System.out.println(cnt);
+	}
+	
+	public void findAll2(Pageable pageable) {
+		Page<ElasticDto2> page = logRepositoy.findAll(pageable);
 	}
 }
